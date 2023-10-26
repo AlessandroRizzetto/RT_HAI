24e3cc52a8560d0d294310e595e4691efaae88d7
Simone <simone.compri@gmail.com>
2023-10-26 18:00:00 +0200

    Tmp commit

--- mediapipe_landmarks.py
+++ mediapipe_landmarks.py
@@ -1,8 +1,11 @@
 
 import os
 import sys
+from MediaPipe import run,getData
+from multiprocessing import Process
 sys.path.append(os.environ.get('PYTHONPATH', ''))
 
+mediapipe_process = Process(target=run)
 
 def getOptions(opts, vars):
 
@@ -25,6 +28,9 @@ def initChannel(name, channel, types, opts, vars):
 
 
 def connect(opts, vars):
+    print("Ciao")
+    mediapipe_process.start()
+    print("Ciaone")
     vars['mp'] = 0
     vars['pos'] = 0
 
@@ -34,18 +40,23 @@ def read(name, sout, reset, board, opts, vars):
     pos = vars['pos']
     mp = vars['mp']
 
-    if name == 'mp':
-        # read the last line of landmarks.stream
-        for n in range(sout.num):
-            f = open("../landmarks.stream", "r")
-            sout[n] = float(f.readlines()[-1])
-            f.close()
+    try:
+        if name == 'mp':
+            # read the last line of landmarks.stream
+            for n in range(sout.num):
+                #f = open("./landmarks.stream", "r")
+                sout[n] = getData() #float(f.readlines()[-1])
+                #print(sout[n])
+                #f.close()
 
-    else:
-        print('unkown channel name')
+        else:
+            print('unkown channel name')
+    except Exception as error:
+        print(error)
 
     vars['pos'] = pos
 
 
 def disconnect(opts, vars):
+    mediapipe_process.kill()
     pass
