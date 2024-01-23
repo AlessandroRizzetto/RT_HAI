52df73d81613ebd195573f93e22e57c929435d39
Simone <simone.compri@gmail.com>
2024-01-23 11:54:44 +0100

    Fixed alpha ratio

--- audio/scripts/alpha_ratio.py
+++ audio/scripts/alpha_ratio.py
@@ -16,12 +16,12 @@ def alpha_ratio(fft_magnitude, sr):
     freqs = librosa.core.fft_frequencies(sr=sr, n_fft=fft_magnitude.shape[0]*2-1)
 
     # Definisci le bande di frequenza
-    low_band = (freqs >= 0) & (freqs < 1000)
+    low_band = (freqs >= 50) & (freqs < 1000)
     high_band = (freqs >= 1000) & (freqs < 5000)
 
     # Calcola l'energia nelle bande di frequenza
-    low_band_energy = np.mean(np.square(fft_magnitude[low_band, :]))
-    high_band_energy = np.mean(np.square(fft_magnitude[high_band, :]))
+    low_band_energy = np.sum(fft_magnitude[low_band, :] ** 2) #np.mean(np.square(fft_magnitude[low_band, :]))
+    high_band_energy = np.sum(fft_magnitude[high_band, :] ** 2) #np.mean(np.square(fft_magnitude[high_band, :]))
 
     # Calcola l'Alpha Ratio
     alpha_ratio = low_band_energy / high_band_energy if high_band_energy != 0 else 0
